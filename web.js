bd54837bd4a6037d7b5a7d2db1254f0567b799a0
gunasundaram <gunasundaram@gmail.com>
2013-07-08 21:24:53 +0000

    Fixed: error in readFileSync

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     fs = require('fs');
-    var fileContent = fs.readSyncFile("Index.html");
+    var fileContent = fs.readFileSync("Index.html");
     var result = fileContent.toString();
     response.send(result);
 });
